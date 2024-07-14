e313590119e33b51b868209ba8ffd27d7fa1aba9
Will Sergeant <12886992+willsarg@users.noreply.github.com>
2024-07-14 00:32:57 -0400

    saving

--- commandArchive.sh
+++ commandArchive.sh
@@ -23,3 +23,32 @@ sudo apt install tree
 
 #File System Consistency Check
 fsck
+
+#Disk Usage
+ls -lh / | sort -k 4
+
+#Note to anyone reading this including future me, this is literally just a scratch pad for commands.
+#Co-Pilot generated mostly.
+
+#Disk Usage Human Readable and Sorted by Size
+du -h | sort -n
+
+#of the root directory
+du -h / | sort -n
+
+#top level only
+du -h --max-depth=1 / | sort -n
+
+#convert everything to MB
+du du -h --max-depth=1 -m / | sort -n
+
+#for every top level directory, tree view the contents of the first secondary folder
+du -h --max-depth=1 / | sort -n | cut -f2 | xargs -I {} du -h --max-depth=1 {}
+
+#use the tree command to display the result
+du -h --max-depth=1 / | sort -n | cut -f2 | xargs -I {} du -h --max-depth=1 {} | tree
+
+# ls / | tree only show the first level of directories
+ls / | tree
+#that didn't work, so let's try this
+ls / | tree -L 1
